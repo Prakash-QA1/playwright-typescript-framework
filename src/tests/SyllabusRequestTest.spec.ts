530f163097056136fad7fdad3e9bb3c944c0abda
Prakash <prakash@freerange.com>
2024-12-06 13:50:08 +0530

    added .env files

--- src/tests/SyllabusRequestTest.spec.ts
+++ src/tests/SyllabusRequestTest.spec.ts
@@ -6,10 +6,17 @@ test("Submit Syllabus Request Form", async ({page}) =>{
     const landingpage = new LandingPage(page);
 
     await landingpage.navigateToLandingPage();
-    await landingpage.fillFirstname("Prakash");
-    await landingpage.fillEmail("prakash+667@ziplines.com");
+    await landingpage.fillFirstname(process.env.firstname!);
+    await landingpage.fillEmail(process.env.email!);
     await landingpage.fillPhone(8956895623);
     const syllabuspage = await landingpage.clickSubmitInput();
     syllabuspage.expectDonwloadSyllabusTextToBeVisible();
 
 })
+
+
+test.skip("Sample ENV test", async ({page}) =>{
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.firstname);
+    console.log(process.env.email);
+});
