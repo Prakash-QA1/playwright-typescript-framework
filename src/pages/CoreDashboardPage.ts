7bdc814dc3d9e59f8c9c2dc65ca212029b07839c
Prakash <prakash@freerange.com>
2024-12-09 17:00:33 +0530

    Added Login with storege State feature.

--- src/pages/CoreDashboardPage.ts
+++ src/pages/CoreDashboardPage.ts
@@ -1,10 +1,16 @@
-import { Page, expect } from "@playwright/test";
+import { Page } from "@playwright/test";
 import logger from "../utils/LoggerUtil";
 
 export default class CoreDashboardPage{
 
     private readonly coreDashboardTitle = "Ziplines Core";
     private readonly loginWithGoogleButtonSelector = "Login with Google";
+
+    private readonly emailInputSelector = "[name='identifier']";
+    private readonly nextButtonSelector = "Next";
+
+    private readonly headingTextSelector = "#headingText";
+    private readonly passwordInputSelector = "[name='Passwd']";
 
     constructor(private page:Page){
 
@@ -16,8 +22,25 @@ export default class CoreDashboardPage{
     }
 
     async clickOnLoginWithGoogleButton(){
-        await this.page.waitForSelector(this.loginWithGoogleButtonSelector),{timeout: 10000};
+
         await this.page.getByText(this.loginWithGoogleButtonSelector).click();
+    }
+
+    async fillEmail(){
+        await this.page.locator(this.emailInputSelector).fill("prakash.lale@ziplines.com");
+    }
+
+    async clickOnNextButton(){
+        await this.page.getByRole('button', { name: 'Next' }).click({timeout:15000});
+    }
+
+    async textIsVisible(){
+        await this.page.locator(this.headingTextSelector).isVisible();
+        console.log("I am on Passsword Page.")
+    }
+
+    async fillPassword(){
+        await this.page.locator(this.passwordInputSelector).fill("Avdi@1212", {timeout:15000});
     }
 
 }
--- src/tests/SyllabusRequestOnCoreTest.spec.ts
+++ src/tests/SyllabusRequestOnCoreTest.spec.ts
@@ -1,18 +1,23 @@
 import { test } from "@playwright/test";
 import CoreDashboardPage from "../pages/CoreDashboardPage";
 
+
+const authFile = "src/config/auth.json";
+
 test("Verify Syllabus Request on CORE.", async ({page}) =>{
 
     const coredashboardpage = new CoreDashboardPage(page);
 
-    try{
         await coredashboardpage.navigateToCoreDashboardPage();
 
         await coredashboardpage.clickOnLoginWithGoogleButton();
+        
+        await coredashboardpage.fillEmail();
+        await coredashboardpage.clickOnNextButton();
 
-    }catch (error)
-    {
-        throw error; // Rethrow to fail the test
-    }
+        await coredashboardpage.fillPassword();
+        await coredashboardpage.clickOnNextButton();
 
+        await page.context().storageState({ path: authFile });
+        
 })
